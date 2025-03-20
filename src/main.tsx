ec81284a3969fc0a24f5f209f4b3f0f11a128619
sexyoung <sexyoung@sexyoungdeMacBook-Pro.local>
2025-03-20 17:50:44 +0800

    chore: setup ga and logrocket

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import LogRocket from 'logrocket';
+import ReactGA from 'react-ga4';
+
 import './index.css'
 import App from './App.tsx'
+
+LogRocket.init('sexyoung/bababa');
+ReactGA.initialize('G-4GESR72G51'); // 請替換成您的 GA4 測量 ID
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
